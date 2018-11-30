7bbf8dde29433ee3587b48c9fe6298dd1cf010de
Ömer Can Danacıoğlu <omercd@hotmail.com.tr>
2018-11-30 18:53:48 +0300

    DbObject -> ToString() Now returns all Properties and Fields that have ToString() method.

--- MongoCRUD/Interfaces/DbObject.cs
+++ MongoCRUD/Interfaces/DbObject.cs
@@ -16,5 +16,28 @@ namespace MongoCRUD.Interfaces
             _id = Guid.NewGuid().ToString();
             CreationDate = DateTime.Now;
         }
+
+        public override string ToString()
+        {
+           
+            var properties = GetType().GetProperties();
+            var fields = GetType().GetFields();
+            StringBuilder returnValue = new StringBuilder();
+            WriteFields(properties);
+            WriteFields(fields);
+            returnValue.Remove(returnValue.Length - 2, 2);
+            void WriteFields(IEnumerable<dynamic> enumerable)
+            {
+                foreach (var property in enumerable)
+                {
+                    var value = property.GetValue(this);
+                    if (value != null)
+                    {
+                        returnValue.Append(property.Name + ":" + value + ", ");
+                    }
+                }
+            }
+            return returnValue.ToString();
+        }
     }
 }
---
+++ MongoCRUDTests/Interfaces/DbObjectTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MongoCRUD.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MongoCRUD.Interfaces.Tests
+{
+    [TestClass()]
+    public class DbObjectTests
+    {
+        private class TestClass:DbObject
+        {
+            public string Name { get; set; }
+            public string Value { get; set; }
+            public string FieldValue;
+        }
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            var testObject = new TestClass
+            {
+                FieldValue = "FieldValue",
+                Value = "PropValue",
+                Name = "PropName",
+                CreationDate = new DateTime(2015,5,13,10,10,10),
+                _id = "TestId"
+            };
+            var result = testObject.ToString();
+            Assert.AreEqual(result, "Name:PropName, Value:PropValue, _id:TestId, CreationDate:15/05/13 10:10:10 AM, FieldValue:FieldValue");
+        }
+    }
+}
